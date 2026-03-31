216fde39244142898275e65b5661fa3a982088cd
Oscar Treviño <otrevinog29@gmail.com>
2026-03-31 15:31:36 -0600

    initial commit

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Home() {
+	return (
+		<div className="container mx-auto max-w-3xl px-4 py-2">
+			<div className="grid gap-6">
+				<h1 className="font-bold text-4xl">Passlet</h1>
+			</div>
+		</div>
+	);
+}
---
+++ packages/ui/src/components/label.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@passlet/ui/lib/utils";
+import type * as React from "react";
+
+function Label({ className, ...props }: React.ComponentProps<"label">) {
+	return (
+		// biome-ignore lint/a11y/noLabelWithoutControl: This is a presentational component and may not have a control.
+		<label
+			className={cn(
+				"flex select-none items-center gap-2 text-xs leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-50 group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50",
+				className
+			)}
+			data-slot="label"
+			{...props}
+		/>
+	);
+}
+
+export { Label };
