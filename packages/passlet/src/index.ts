835f831c7df5df92e4c6a6ce92c0ef5a0179601a
Oscar Treviño <otrevinog29@gmail.com>
2026-04-01 17:38:48 -0600

    feat: export public API surface

---
+++ packages/passlet/src/index.ts
@@ -0,0 +1,28 @@
+export type { WalletErrorCode } from "./errors";
+export { WALLET_ERROR_CODES, WalletError } from "./errors";
+export { field, Pass } from "./pass";
+export type {
+	AppleCredentials,
+	GoogleCredentials,
+	IssuedPass,
+	WalletCredentials,
+} from "./types/credentials";
+export type {
+	Barcode,
+	BarcodeFormat,
+	CouponPassConfig,
+	CreateConfig,
+	DateStyle,
+	EventPassConfig,
+	FieldDef,
+	FlightPassConfig,
+	GenericPassConfig,
+	GiftCardPassConfig,
+	ImageSet,
+	ImageSource,
+	LoyaltyPassConfig,
+	NumberStyle,
+	PassConfig,
+	TextAlignment,
+} from "./types/schemas";
+export { Wallet } from "./wallet";
